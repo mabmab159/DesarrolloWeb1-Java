939d1eac25f9320890345a20201d1deebe036f0f
MabMab^^ <miguelberrioh@hotmail.com>
2025-11-10 21:17:17 -0500

    proyecto base

---
+++ clase1/src/main/java/com/cibertec/clase1/entidades/Alumno.java
@@ -0,0 +1,18 @@
+package com.cibertec.clase1.entidades;
+
+import jakarta.persistence.*;
+
+@Entity(name = "AlumnosTabla")
+@Table(name = "Alumnos")
+public class Alumno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String nombre;
+    @Column(name = "lastName")
+    private String apellido;
+    private String carrera;
+    private Long codigoEstudiante;
+    private double promedioNotas;
+}
