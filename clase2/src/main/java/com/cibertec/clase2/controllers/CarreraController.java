33fd47591ccda42a3870bb0889223386941acd65
MabMab^^ <miguelberrioh@hotmail.com>
2025-11-24 20:01:51 -0500

    Filtro por manera 2

--- clase2/src/main/java/com/cibertec/clase2/controllers/CarreraController.java
+++ clase2/src/main/java/com/cibertec/clase2/controllers/CarreraController.java
@@ -1,11 +1,15 @@
 package com.cibertec.clase2.controllers;
 
+import com.cibertec.clase2.models.Carrera;
 import com.cibertec.clase2.services.CarreraService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Controller
 @RequestMapping("/carrera")
@@ -26,6 +30,11 @@ public class CarreraController {
             model.addAttribute("carreras", carreraService.findByCarrera(keyword));
             System.out.println(carreraService.findByCarrera(keyword));
             //2. Filtrar en memoria - Solo usarlo cuando son pocos datos en la BD
+            /*Iterable<Carrera> carreraIterable = carreraService.findAll();
+            List<Carrera> carrerasFiltradas = StreamSupport.stream(carreraIterable.spliterator(), false)
+                    .filter(c -> c.getNombreCarrera().toLowerCase().equals(keyword.toLowerCase()))
+                    .toList();
+            model.addAttribute("carreras", carrerasFiltradas);*/
         } else {
             model.addAttribute("carreras", carreraService.findAll());
         }
